0e4c65e36a60fd83e0ed661fe6d824bfdff14eaf
Benjamin Hell <beni@siebengang.net>
2012-11-25 13:28:48 +0100

    Enabled admindocs

--- jimi/jimi/settings.py
+++ jimi/jimi/settings.py
@@ -111,7 +111,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'jimi.catalog',
     'jimi.price',
     'jimi.lists',
